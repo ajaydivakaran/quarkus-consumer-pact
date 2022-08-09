fdd259263297d2fb3895e99a0e6a6c741e6e7db1
Ajay <ajaydi@thoughtworks.com>
2022-08-10 01:36:40 +0530

    Updated provider name

--- src/test/java/me/spike/ConsumerPactTest.java
+++ src/test/java/me/spike/ConsumerPactTest.java
@@ -5,7 +5,6 @@ import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.PactSpecVersion;
-import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import io.quarkus.test.junit.QuarkusTest;
@@ -25,7 +24,7 @@ public class ConsumerPactTest {
     @RestClient
     GreetingRestClient greetingService;
 
-    @Pact(provider = "Provider", consumer = "Consumer")
+    @Pact(provider = "springboot-provider-pact", consumer = "Consumer")
     public V4Pact greet(PactDslWithProvider builder) {
         return builder
                 .uponReceiving("greet")
